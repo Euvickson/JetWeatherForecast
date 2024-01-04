89017321cc65077b28d163aca441a8b302b44f29
Euvickson <victor.eugennio@gmail.com>
2024-01-04 12:38:11 -0300

    Started the Navigation settings
    TODO: Need to Create the Splash screen logic

---
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/navigation/WeatherNavigation.kt
@@ -0,0 +1,17 @@
+package br.com.euvickson.jetweatherforecast.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.euvickson.jetweatherforecast.screens.WeatherSplashScreen
+
+@Composable
+fun WeatherNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
+        composable(WeatherScreens.SplashScreen.name) {
+            WeatherSplashScreen(navController)
+        }
+    }
+}
---
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -0,0 +1,10 @@
+package br.com.euvickson.jetweatherforecast.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherSplashScreen(navController: NavController) {
+    Text(text = "SplashScreen")
+}
