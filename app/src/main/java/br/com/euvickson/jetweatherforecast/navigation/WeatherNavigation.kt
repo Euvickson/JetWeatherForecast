1307dd53191cb49a7deddde4084735ca6475c9b1
Euvickson <victor.eugennio@gmail.com>
2024-01-05 12:50:27 -0300

    - Added some Animation to the Splash Screen and created the logic so it can go to the next screen once the animation ends
    - Created the MainScreen logic
    
    TODO: Create the MainScreen UI

--- app/src/main/java/br/com/euvickson/jetweatherforecast/navigation/WeatherNavigation.kt
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/navigation/WeatherNavigation.kt
@@ -4,14 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import br.com.euvickson.jetweatherforecast.screens.MainScreen
 import br.com.euvickson.jetweatherforecast.screens.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
+
         composable(WeatherScreens.SplashScreen.name) {
             WeatherSplashScreen(navController)
+        }
+
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController)
         }
     }
 }
--- app/src/main/java/br/com/euvickson/jetweatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -1,5 +1,8 @@
 package br.com.euvickson.jetweatherforecast.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -11,20 +14,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import br.com.euvickson.jetweatherforecast.R
+import br.com.euvickson.jetweatherforecast.navigation.WeatherScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
+
+    val scale = remember {
+        Animatable(initialValue = 0f)
+    }
+
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(targetValue = 0.9f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = { OvershootInterpolator(6f).getInterpolation(it) }
+            )
+        )
+        delay(2000L)
+        navController.navigate(route = WeatherScreens.MainScreen.name)
+    })
+
     Surface (modifier = Modifier
         .padding(15.dp)
-        .size(330.dp),
+        .size(330.dp)
+        .scale(scale = scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
@@ -36,7 +61,11 @@ fun WeatherSplashScreen(navController: NavController) {
             Image(painter = painterResource(id = R.drawable.sun), contentDescription = "Sunny icon",
                 contentScale = ContentScale.Fit,
                 modifier = Modifier.size(95.dp))
-            Text(text = "Find the Sun?", style = MaterialTheme.typography.titleLarge, color = Color.LightGray)
+
+            Text(
+                text = "Find the Sun?",
+                style = MaterialTheme.typography.titleLarge,
+                color = Color.LightGray)
 
         }
     }
