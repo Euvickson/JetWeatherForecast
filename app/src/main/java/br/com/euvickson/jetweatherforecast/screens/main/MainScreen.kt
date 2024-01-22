56ad6e84a18d89beee82a802e5d9e4ea733c50aa
Euvickson <victor.eugennio@gmail.com>
2024-01-22 12:53:43 -0300

    - Getting the Icon from the API
    - Started some components in the MainScreen.kt

--- app/src/main/java/br/com/euvickson/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,23 +1,37 @@
 package br.com.euvickson.jetweatherforecast.screens.main
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import br.com.euvickson.jetweatherforecast.data.DataOrException
 import br.com.euvickson.jetweatherforecast.model.Weather
 import br.com.euvickson.jetweatherforecast.widgets.WeatherAppBar
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 
 @Composable
 fun MainScreen(
@@ -33,7 +47,7 @@ fun MainScreen(
 
     if (weatherData.loading == true) {
         CircularProgressIndicator()
-    } else if (weatherData.data != null){
+    } else if (weatherData.data != null) {
         MainScraffold(weather = weatherData.data!!, navController = navController)
     }
 
@@ -43,7 +57,7 @@ fun MainScreen(
 @Composable
 fun MainScraffold(weather: Weather, navController: NavController) {
 
-    Scaffold (
+    Scaffold(
         topBar = {
             WeatherAppBar(
                 title = weather.city.name + ", ${weather.city.country}",
@@ -54,15 +68,65 @@ fun MainScraffold(weather: Weather, navController: NavController) {
             }
         }
     ) {
-        Column (modifier = Modifier.padding(it)){
+        Column(modifier = Modifier.padding(it)) {
             MainContent(data = weather)
         }
     }
-    
+
 
 }
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(text = data.city.name)
+
+    val imageUrl = "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+            Text(
+                text = "Jan 22",
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(6.dp)
+            )
+
+            Surface(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(200.dp),
+                shape = CircleShape,
+                color = Color(0xFFFFC400)
+            ) {
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    //https://openweathermap.org/img/wn/10d.png
+                    WeatherStateImage(imageUrl = imageUrl)
+
+                    Text(text = "56", style = MaterialTheme.typography.headlineLarge, fontWeight = FontWeight.ExtraBold)
+                    Text(text = "Snow", fontStyle = FontStyle.Italic, fontSize = 20.sp)
+
+                }
+
+            }
+
+
+
+    }
+
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    AsyncImage(model = imageUrl, contentDescription = "Icon Image", modifier = Modifier.size(80.dp))
 }
