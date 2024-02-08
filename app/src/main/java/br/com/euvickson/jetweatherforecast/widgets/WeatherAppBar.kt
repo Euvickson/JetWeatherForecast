25acf5a685078073a8bc867fd27d5cd9947b1ea8
Euvickson <victor.eugennio@gmail.com>
2024-02-08 20:54:09 -0300

    - Setting up the Dialog and Dropdown menu
    (Not Finished)

--- app/src/main/java/br/com/euvickson/jetweatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/widgets/WeatherAppBar.kt
@@ -2,10 +2,20 @@ package br.com.euvickson.jetweatherforecast.widgets
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,6 +24,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -38,6 +54,13 @@ fun WeatherAppBar(
     onButtonClicked: () -> Unit = {}
 ) {
 
+    val showDialog = remember { mutableStateOf(false) }
+
+    if (showDialog.value) {
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+
+    }
+
     TopAppBar(
         title = {
             Text(
@@ -57,7 +80,9 @@ fun WeatherAppBar(
                     )
                 }
 
-                IconButton(onClick = { onButtonClicked.invoke() }) {
+                IconButton(onClick = {
+                    showDialog.value = true
+                }) {
                     Icon(
                         imageVector = Icons.Rounded.MoreVert,
                         contentDescription = "More Icon"
@@ -78,4 +103,53 @@ fun WeatherAppBar(
         modifier = Modifier.shadow(elevation = elevation)
     )
 
+}
+
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+
+    var expanded by remember { mutableStateOf(true) }
+    val items = listOf("About", "Favorite", "Settings")
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.width(140.dp)
+        ) {
+
+            items.forEachIndexed { _, text ->
+
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+                        showDialog.value = false
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = when (text) {
+                                "About" -> Icons.Default.Info
+                                "Favorites" -> Icons.Default.FavoriteBorder
+                                else -> Icons.Default.Settings
+
+                            },
+                            contentDescription = null,
+                            tint = Color.LightGray
+                        )
+                    },
+                    text = {
+                        Text(text = text, fontWeight = FontWeight.W300)
+                    },
+                    modifier = Modifier.clickable {
+
+                    })
+
+            }
+        }
+    }
 }
