c26ac66b0d2419a2cf7fe7c6dd641d2b2318c427
Euvickson <victor.eugennio@gmail.com>
2024-02-13 18:52:45 -0300

    - Created the WeatherDao.kt and some functions that the app it's gonna use

---
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/data/WeatherDao.kt
@@ -0,0 +1,33 @@
+package br.com.euvickson.jetweatherforecast.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import br.com.euvickson.jetweatherforecast.model.Favorite
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface WeatherDao {
+
+    @Query("SELECT * FROM FAV_TBL")
+    fun getFavorites(): Flow<List<Favorite>>
+
+    @Query("SELECT * FROM FAV_TBL WHERE city =:city")
+    suspend fun getFavById(city: String): Favorite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: Favorite)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavorite(favorite: Favorite)
+
+    @Query("DELETE FROM fav_tbl")
+    suspend fun deleteAllFavorites()
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite)
+}
