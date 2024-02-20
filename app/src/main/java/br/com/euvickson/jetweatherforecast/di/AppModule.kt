4538628895cb3ae3b27609f47556436b70f2d66c
Euvickson <victor.eugennio@gmail.com>
2024-02-19 21:22:53 -0300

    - Created the AppModule Functions that will create the Room Data Base and the Weather Data Access Object

--- app/src/main/java/br/com/euvickson/jetweatherforecast/di/AppModule.kt
+++ app/src/main/java/br/com/euvickson/jetweatherforecast/di/AppModule.kt
@@ -1,10 +1,15 @@
 package br.com.euvickson.jetweatherforecast.di
 
+import android.content.Context
+import androidx.room.Room
+import br.com.euvickson.jetweatherforecast.data.WeatherDao
+import br.com.euvickson.jetweatherforecast.data.WeatherDatabase
 import br.com.euvickson.jetweatherforecast.network.WeatherApi
 import br.com.euvickson.jetweatherforecast.utils.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -23,4 +28,17 @@ class AppModule {
             .build()
             .create(WeatherApi::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao =
+        weatherDatabase.weatherDao()
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): WeatherDatabase =
+        Room
+            .databaseBuilder(context, WeatherDatabase::class.java, "weather_database")
+            .fallbackToDestructiveMigration()
+            .build()
 }
